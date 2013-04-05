a5f3aa08c0ae24480f4e1a4e57b9886951bc5d52
Darío Blanco <dario.blanco@edelight.de>
2013-04-05 21:06:03 +0200

    Fix pelican settings

--- thesoftjaguar/pelicanconf.py
+++ thesoftjaguar/pelicanconf.py
@@ -9,21 +9,18 @@ TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = u'en'
 
-# Blogroll
-LINKS = (('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('twitter', 'http://twitter.com/darioblanco'),
-          ('facebook', 'https://www.facebook.com/dario.blancoit'),
-          ('linkedin', 'http://www.linkedin.com/in/darioblancoiturriaga'),
-          ('github', 'http://github.com/sharkerz'),)
-
 DEFAULT_PAGINATION = 10
 
-GITHUB_URL = 'http://github.com/sharkerz/'
-FEED_ALL_RSS = 'feeds/all.rss.xml'
-CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
 THEME = 'bootstrap-theme'
+
 TEMPLATE_PAGES = {
     'projects.html': 'projects.html',
 }
+
+# Feeds
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
+TAG_FEED_ATOM = 'feeds/%s.atom.xml'
+TAG_FEED_RSS = 'feeds/%s.rss.xml'
