7e7896c07e1c7ef220a4d968b81a2c09e94bf385
Darío Blanco <dario.blanco@edelight.de>
2013-04-06 05:07:59 +0200

    Modify pelican config file

--- thesoftjaguar/pelicanconf.py
+++ thesoftjaguar/pelicanconf.py
@@ -7,6 +7,8 @@ SITEURL = 'http://thesoftjaguar.com'
 
 TIMEZONE = 'Europe/Paris'
 
+DEFAULT_DATE_FORMAT = '%d %b, %Y'
+
 DEFAULT_LANG = u'en'
 
 DEFAULT_PAGINATION = 10
@@ -16,6 +18,12 @@ THEME = 'bootstrap-theme'
 TEMPLATE_PAGES = {
     'projects.html': 'projects.html',
 }
+
+# Urls
+ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+YEAR_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/index.html'
 
 # Feeds
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
