6f54bcb98eb0f78848691948c6348daf333926ac
Darío Blanco <dario.blanco@edelight.de>
2014-01-24 17:56:23 +0100

    Edit production configuration

--- thesoftjaguar/publishconf.py
+++ thesoftjaguar/publishconf.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+# This file is only used if you use `make publish` or
+# explicitly specify it as your config file.
 
 import sys
 sys.path.append('.')
@@ -9,10 +13,15 @@ SITEURL = 'http://thesoftjaguar.com'
 
 DELETE_OUTPUT_DIRECTORY = True
 
-# Following items are often useful when publishing
-
-# Uncomment following line for absolute URLs in production:
-#RELATIVE_URLS = False
+RELATIVE_URLS = False
 
 DISQUS_SITENAME = "thesoftjaguar"
 GOOGLE_ANALYTICS = "UA-27981742-1"
+
+# Feeds
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_RSS = None
+TAG_FEED_RSS = None
